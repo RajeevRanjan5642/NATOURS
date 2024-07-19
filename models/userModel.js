b618cd45b6c88abdfef4bea500c9adc165fd2cb5
RajeevRanjan5642 <rajeevranjan31012003@gmail.com>
2024-07-19 21:25:26 +0530

    add

---
+++ models/userModel.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+//name,email,photo, password, password confirm
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name!'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email!'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+
+  photo: String,
+
+  password: {
+    type: String,
+    required: [true, 'Please provide a password!'],
+    minlength: 8,
+  },
+
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate:{
+        // This only works on CREATE and SAVE !!!
+        validator:function(el){
+            return el === this.password;
+        },
+        message: 'Passwords are not the same',
+    }
+  },
+});
+
+userSchema.pre('save',async function(next){
+    if(!this.isModified('password')) return next();
+
+    //encryption (cost 12)
+    this.password = await bcrypt.hash(this.password,12);
+    
+    this.passwordConfirm = undefined;
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
